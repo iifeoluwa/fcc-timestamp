c6a2c56fafcf62564d3d1850db420921eb7dbaa3
Arowosegbe Ifeoluwa <iifeoluwa.ao@gmail.com>
2017-02-27 02:12:32 +0100

    Modified file structure

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const http = require('http');
-const timestamp = require('./verify_timestamp')
+const timestamp = require('./src/verify_timestamp')
 
 http.createServer((req, res) => {
-	
-	let url = unescape(req.url.replace('/', ''));
-	let result = timestamp(url);
-	
-	res.setHeader('Content-Type', 'application/json');
-	
-    res.write(JSON.stringify(result));
-	res.end();
+
+	if (req.url == '/') {
+
+	}else{
+		let url = unescape(req.url.replace('/', ''));
+		let result = timestamp(url);
+		
+		res.setHeader('Content-Type', 'application/json');
+		
+	    res.write(JSON.stringify(result));
+		res.end();
+	}
 
 }).listen(3333)
 
